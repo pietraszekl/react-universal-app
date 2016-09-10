73bdcfa1f59ab14269b90d0df2622386f380c8ae
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-09-10 13:42:21 +0100

    added main app files

---
+++ src/README.md
@@ -0,0 +1,9 @@
+Universal React App
+
+npm install
+
+start local server:
+NODE_ENV=production node_modules/.bin/babel-node --presets 'react,es2015' src/server.js
+
+
+app is available at http://localhost:3000
---
+++ src/components/IndexPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AthletePreview from './AthletePreview';
+import athletes from '../data/athletes';
+
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="athletes-selector">
+          {athletes.map(athleteData => <AthletePreview key={athleteData.id} {...athleteData} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
